7c7bd400281693e9b20acf768bfb581ac70b8985
Luiz Felipe <luizfelipecrx@gmail.com>
2023-09-28 00:07:47 -0300

    Operações com inteiros

--- exercicio03.cpp
+++ exercicio03.cpp
@@ -13,9 +13,14 @@ int main()
     std::cout << "Resultado em formato hexadecimal: " << std::hex << c << std::endl;
     std::cout << "Resultado em formato octal: " << std::oct << c << std::endl;
 
-    c = std::abs(a - b); // atribuir o módulo da diferença entre a e b à variável c
+    c = std::abs(a - b);
 
-    std::cout << "Resultado: " << c << std::endl; // imprimir o resultado
+    std::cout << "Módulo da diferença entre a e b: " << c << std::endl;
+
+    c = (b != 0) ? (a / b) : 0;
+    std::cout << "Quociente entre a e b: " << c << std::endl;
+
+    std::cout << ((a % b == 0) ? "a é divisível de forma exata por b." : "a não é divisível de forma exata por b.") << std::endl;
 
     return 0;
 }
