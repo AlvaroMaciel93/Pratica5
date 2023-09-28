fa11031e08fa17e2449fd9341781f5d6971afccb
Luiz Felipe <luizfelipecrx@gmail.com>
2023-09-28 00:21:40 -0300

    Implementação da segunda parte do exercício 4

--- exercicio04v2.cpp
+++ exercicio04v2.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <cmath>
 
-int main() {
+int main()
+{
     double a, b, c;
     std::cout << "Digite os coeficientes a, b e c do polinômio de segundo grau: ";
     std::cin >> a >> b >> c;
 
     double discriminante = b * b - 4 * a * c;
-    if (discriminante > 0) {
+    if (discriminante > 0)
+    {
         double raiz1 = (-b + std::sqrt(discriminante)) / (2 * a);
         double raiz2 = (-b - std::sqrt(discriminante)) / (2 * a);
         std::cout << "O polinômio tem duas raízes reais: " << raiz1 << " e " << raiz2 << std::endl;
-    } else if (discriminante == 0) {
+    }
+    else if (discriminante == 0)
+    {
         double raiz = -b / (2 * a);
         std::cout << "O polinômio tem uma raiz real: " << raiz << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "O polinômio não tem raízes reais." << std::endl;
     }
 
