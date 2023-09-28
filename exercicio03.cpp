4bf9dd649157fc44038d3593d717e5192970530e
Luiz Felipe <luizfelipecrx@gmail.com>
2023-09-28 00:00:59 -0300

    Implementa operações com o atributo c

--- exercicio03.cpp
+++ exercicio03.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <iomanip>
 
-int main() {
+int main()
+{
     int a, b, c;
 
     std::cout << "Digite dois números inteiros: ";
@@ -10,6 +11,11 @@ int main() {
     c = a + b;
 
     std::cout << "Resultado em formato hexadecimal: " << std::hex << c << std::endl;
+    std::cout << "Resultado em formato octal: " << std::oct << c << std::endl;
+
+    c = std::abs(a - b); // atribuir o módulo da diferença entre a e b à variável c
+
+    std::cout << "Resultado: " << c << std::endl; // imprimir o resultado
 
     return 0;
 }
