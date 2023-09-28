22becab8fc1cd92eed333515786ab43e137f24d9
Luiz Felipe <luizfelipecrx@gmail.com>
2023-09-27 23:50:36 -0300

    Imprimir ch3 em diversos formatos

--- exercicio02.cpp
+++ exercicio02.cpp
@@ -7,6 +7,8 @@ int main()
     std::cout << "Digite um caractere: ";
     std::cin >> ch1;
 
+    std::cout << std::endl;
+
     /*  Você pode usar as funções isupper,
      islower e isdigit para verificar se
      um caractere é uma letra maiúscula,
@@ -31,11 +33,22 @@ int main()
 
     std::cout << "O caractere digitado é " << type << std::endl;
 
+    std::cout << std::endl;
+
     ch2 = 81;
     std::cout << "Caractere em formato numérico decimal: " << static_cast<int>(ch2) << std::endl;
     std::cout << "Caractere em formato numérico octal: " << std::oct << static_cast<int>(ch2) << std::endl;
     std::cout << "Caractere em formato numérico hexadecimal: " << std::hex << static_cast<int>(ch2) << std::endl;
     std::cout << "Caractere como caractere: " << ch2 << std::endl;
+
+    std::cout << std::endl;
+
+    // A diferença de valor entre uma letra maiúscula e sua correspondente minúscula é sempre 32.
+    ch3 = ch2 + 32;
+    std::cout << "Caractere em formato decimal: " << static_cast<int>(ch3) << std::endl;
+    std::cout << "Caractere em formato octal: " << std::oct << static_cast<int>(ch3) << std::endl;
+    std::cout << "Caractere em formato hexadecimal: " << std::hex << static_cast<int>(ch3) << std::endl;
+    std::cout << "Caractere: " << ch3 << std::endl;
 
     return 0;
 }
